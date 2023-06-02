f0ca29da6963f5d6370ff8594aa6d3c7ae830622
caxtonMuthoni <githinjicaxton323@gmail.com>
2023-06-02 11:38:46 +0300

    modified button trigger

--- js/index.js
+++ js/index.js
@@ -1,21 +1,26 @@
-Notification.requestPermission().then(function(permission) {
+Notification.requestPermission().then(function (permission) {
     if (permission == 'granted') {
         let options = {
             body: 'You have allowed notifications for this site',
             icon: '../images/notification.png'
         };
 
-       const notification = new Notification("Congratulations !", options);
+        const notification = new Notification("Congratulations !", options);
     }
 });
 
+navigator.serviceWorker.register('sw.js');
 const button = document.getElementById('notificationButtuonID');
-button.addEventListener('click', function(e) {
+button.addEventListener('click', function (e) {
     e.preventDefault();
-    let options = {
-        body: 'This notification was triggered by the button click',
-        icon: '../images/notification.png'
-    };
+    //     let options = {
+    //         body: 'This notification was triggered by the button click',
+    //         icon: '../images/notification.png'
+    //     };
 
-   const notification = new Notification("Button Notification", options);
+    //    const notification = new Notification("Button Notification", options);
+
+    navigator.serviceWorker.ready.then(function(registration) {
+        registration.showNotification('Notification with ServiceWorker');
+      });
 })
